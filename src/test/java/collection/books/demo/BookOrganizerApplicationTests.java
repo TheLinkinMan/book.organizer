d7167dc2f71b44e0fa7895ce1c9ebfda5f25a31b
TheLinkinMan <alexandrlinkman@gmail.com>
2021-08-03 12:42:20 +0300

    initial commit

---
+++ src/test/java/collection/books/demo/BookOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package collection.books.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
